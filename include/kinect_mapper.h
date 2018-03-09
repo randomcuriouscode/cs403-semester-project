75825fe4aa9cdcf7f4a518c8aa29a097e91effff
rpg711 <zijian1994@gmail.com>
2018-03-09 11:25:26 -0800

    adding kinect mapper prototypes

--- include/kinect_mapper.h
+++ include/kinect_mapper.h
@@ -1,13 +1,21 @@
 #pragma once
-
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Eigenvalues>
+#include <geometry_msgs/Point32.h>
 /*
   All function and class declarations pertaining to kinect mapping goes here.
   All names declared in the namespace must be unique.
 */
 
+using namespace std;
+using geometry_msgs::Point32;
+
 namespace followlib 
 {
   // all code goes here
+
+  void trans_p(Point32 &p_o, Eigen::Vector3f &p_trans);
+  void trans_pc(vector<Point32> &points, vector<Point32> &points_translated);
 
   /*
     A trivial test function, remove when stuff is implemented here.
