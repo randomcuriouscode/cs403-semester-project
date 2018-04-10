e95da6199284d329eac826e4cbe47d4f9f4fa099
quoccuong042009 <quoccuong042009@yahoo.com>
2018-04-10 16:38:13 -0400

    update pathfinding.h and pathfinding.cpp

--- include/pathfinding.h
+++ include/pathfinding.h
@@ -1,11 +1,47 @@
 #pragma once
 
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+#include <math.h>
+
+#include "follow_core.h"
 /*
   All function and class declarations pertaining to pathfinding goes here.
   All names declared in the namespace must be unique.
 */
 
-namespace followlib 
+
+namespace followlib
 {
   // all code goes here
+  class PathFinding{
+  private:
+    turtlesim::Pose initLocation;
+    turtlesim::Pose finalLocation;
+    double distance_tolerance;
+    ros::Publisher cmd_vel_pub;
+    ros::Subscriber pose_sub;
+    ros::NodeHandle nh;
+
+  public:
+    /*
+      goal: final location to reach
+      n: NodeHandle
+    */
+    PathFinding(turtlesim::Pose goal, double d, ros::NodeHandle &n);
+    /*
+      helper function fo get distance between 2 points
+    */
+    double getDistance(double x1, double y1, double x2,double y2);
+
+    /*
+      move to finalLocation
+    */
+    void moveGoal();
+
+    /*
+      get current location of the turtleBot
+    */
+    void poseCallBack(const turtlesim::Pose &p_msg);
+  };
 }
--- src/libs/pathfinding.cpp
+++ src/libs/pathfinding.cpp
@@ -1,1 +1,47 @@
 #include "pathfinding.h"
+
+followlib::PathFinding::PathFinding(turtlesim::Pose goal, double d, ros::NodeHandle &n){
+  finalLocation = goal;
+  nh = n;
+  distance_tolerance = d;
+  cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/pathfinding/cmd_vel", 1);
+  pose_sub = n.subscribe("/pose_vel", 1000, &PathFinding::poseCallBack,this);
+
+}
+void followlib::PathFinding::poseCallBack(const turtlesim::Pose &p_msg){
+  initLocation.x = p_msg.x;
+  initLocation.y = p_msg.y;
+  initLocation.theta = p_msg.theta;
+}
+
+double followlib::PathFinding::getDistance(double x1, double y1, double x2,double y2){
+  return sqrt(pow((x1-x2),2) + pow((y1-y2),2));
+}
+
+void followlib::PathFinding::moveGoal(){
+  geometry_msgs::Twist vel_msg;
+
+  ros::Rate loop_rate(10);
+
+  do{
+    vel_msg.linear.x = 1.5*getDistance(initLocation.x,initLocation.y,finalLocation.x,finalLocation.y);
+    vel_msg.linear.y = 0;
+    vel_msg.linear.z = 0;
+
+    vel_msg.angular.x = 0;
+    vel_msg.angular.y = 0;
+    vel_msg.angular.z = 4*(atan2(finalLocation.y-initLocation.y,finalLocation.x-initLocation.x) - initLocation.theta);
+
+    cmd_vel_pub.publish(vel_msg);
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  while(getDistance(initLocation.x,initLocation.y,finalLocation.x,finalLocation.y) > distance_tolerance);
+
+  vel_msg.linear.x = 0;
+  vel_msg.angular.z = 0;
+
+  cmd_vel_pub.publish(vel_msg);
+
+}
